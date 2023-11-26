d4dfaa085eee04c94820b16f5a75cd382fa2f51b
alina <alinabagwan57@gmail.com>
2023-11-26 15:07:40 +0530

    docker build and push

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,10 @@
+def call(String dockerHubUsername, String imageName) {
+    // Build the Docker image
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=5a06e15c97msh69165950b07f3d6p1cc087jsn9945ca5c12a7 -t ${youtube} ."
+     // Tag the Docker image
+    sh "docker tag ${youtube} ${alinabagwan12345}/${youtube}:latest"
+    // Push the Docker image
+    withDockerRegistry([url: 'https://index.docker.io/v1/', credentialsId: 'docker']) {
+        sh "docker push ${alinabagwan12345}/${youtube}:latest"
+    }
+}
